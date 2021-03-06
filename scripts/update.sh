99e6438f79374f1ad6e600ea53e15d24e8c34e79
Xarpe <8762247+Xarpe@user.noreply.gitee.com>
2021-03-06 12:34:47 +0800

    update

--- scripts/update.sh
+++
@@ -1,8 +0,0 @@
-# ./unpack.sh
-dpkg-scanpackages -m . /dev/null > Packages
-bzip2 -c Packages > Packages.bz2
-gzip  -c Packages > Packages.gz
-
-sed "s/{{DATE}}/$(date '+%d-%m-%y %T')/g" <README_TEMPLATE.MD >README.MD
-
-./scripts/format_readme.py
